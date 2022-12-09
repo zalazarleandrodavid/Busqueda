a5eba5f9d159815219558f19209a743252a8d539
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2022-12-08 22:57:05 -0300

    proceso

---
+++ src/components/search/search.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Search() {
+  return (
+    <>
+    <h1>Buscador de Noticia</h1>
+      <div>
+        <div class="input-group mb-3">
+
+          <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+        </div>
+      </div>
+
+    </>
+  );
+}
+
+export default Search;
+
