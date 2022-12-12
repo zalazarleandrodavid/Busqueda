278ae1e0702c442d8dc41fcb592cf8d15f675628
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2022-12-12 20:56:27 -0300

    Search y paginador

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -3,35 +3,46 @@ import { useEffect, useState } from 'react';
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
+import Paginador from "../paginador/Paginador";
 
 
 function Search() {
 
-  /*   const [usuarios, setUsuarios] = useState([]);
+    const [usuarios, setUsuarios] = useState([]);
     const [busqueda, setBusqueda] = useState("");
   
     const peticionGet = async () => {
-      await axios.get(`Datos`)
+      await axios.get(`https://newsapi.org/v2/everything?q=${busqueda}&apiKey=3e3a42ae65bf4c1eb11a815c85fe7c0e&page=1&language=es&pageSize=10&language=es`)
         .then(response => {
           setUsuarios(response.data);
         }).catch(error => {
-          console.log(error);
+          //console.log(error);
         })
     }
   
     useEffect(() => {
       peticionGet();
-    }, []) */
-  const [usuarios, setUsuarios] = useState([]);
-  const [busqueda, setBusqueda] = useState("portugal");
+    }, []) 
+
+  const url=(`https://newsapi.org/v2/everything?q=${busqueda}&apiKey=3e3a42ae65bf4c1eb11a815c85fe7c0e&page=1&language=es&pageSize=10&language=es`)
   
-  const url=(`https://newsapi.org/v2/everything?q=${busqueda}&apiKey=3e3a42ae65bf4c1eb11a815c85fe7c0e&page=1&language=es`)
     const showdata =async() =>{
       const response = await fetch(url)
       const data = await response.json()
-      console.log(data)
+      //console.log(data)
     }
     showdata()
+    
+    const totalResults = url
+    const pagina = Math.ceil(parseInt(totalResults)/10)
+    
+
+    const increment= () => {
+      for (let index = 0; index < totalResults.length; index+=20) {
+        const element = totalResults[index];
+        
+      }
+    }
 
   return (
     <>
@@ -45,7 +56,7 @@ function Search() {
           />
           {busqueda.length >= 3 &&
 
-            <Button>buscar</Button>
+            <Button >buscar</Button>
           }
 
 
@@ -64,4 +75,5 @@ function Search() {
 
 
 export default Search;
+
 
