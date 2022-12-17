6b5fc177e218e8ef953b9fc89fc4f1147bdfe541
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2022-12-17 09:14:15 -0300

    prueba de busqueda

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -6,71 +6,49 @@ import { Button } from "react-bootstrap";
 import Paginador from "../paginador/Paginador";
 import noticias from "../noticias/Noticias";
 
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
 
-function Search() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Realizar la búsqueda aquí utilizando la API de NewsAPI
+  }
 
-  const [usuarios, setUsuarios] = useState([]);
-  const [tablaUsuarios, setTablaUsuarios]= useState([])
-  const [busqueda, setBusqueda] = useState("");
-  
-    const peticionGet = async () => {
-      await axios.get(`https://newsapi.org/v2/everything?q=${busqueda}&apiKey=93ec34e03d6f4d769b150cc027fc47ee&page=1&language=es&pageSize=10&language=es`)
-        .then(response => {
-          setUsuarios(response.data);
-          setTablaUsuarios(response.data)
-        }).catch(error => {
-        console.log(error);
-        })
-    }
-    useEffect(() => {
-      peticionGet();
-    }, []) 
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=93ec34e03d6f4d769b150cc027fc47ee&page=1&language=es&pageSize=10&language=es`
+      );
+      setSearchResults(result.data.articles);
+    };
+    fetchData();
+  }, [searchTerm]);
 
-    const handleChange=e=>{
-      setBusqueda(e.target.value);
-    }
-    //const url=(`https://newsapi.org/v2/everything?q=${busqueda}&apiKey=93ec34e03d6f4d769b150cc027fc47ee&page=1&language=es&pageSize=10&language=es`)
-  
-    //const showdata =async() =>{
-      //const response = await fetch(url)
-      //const data = await response.json()
-      //console.log(data)
-    //}
-    //showdata()
-  
-
-      
   return (
-    <>
-      <h1>Buscador de Noticia</h1>
-      <div>
-        <div className="containerInput">
-
-          <input type="text" className="form-control inputBuscar" placeholder="Buscar Noticia" aria-label="Username" aria-describedby="basic-addon1"
-            value={busqueda}
-            onChange={handleChange}
-
-          />
-          {busqueda.length >= 3 &&
-
-            <Button onClick={peticionGet}>buscar</Button>
-          }
-          
-
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {searchResults.map((result) => (
+        <div key={result.title}>
+          <h3>{result.title}</h3>
+          <p>{result.description}</p>
+          <a href={result.url}>Read more</a>
         </div>
-
-        <div>
-
-        </div>
-
-      </div>
-
-    </>
+      ))}
+    </div>
   );
-}
-
-
+};
 
 export default Search;
 
