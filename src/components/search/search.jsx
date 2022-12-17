a194a8c7306a6b7f8ee185ff582eda1d912149f6
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2022-12-17 10:02:14 -0300

    pruebas 1

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -5,10 +5,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
 import Paginador from "../paginador/Paginador";
 import noticias from "../noticias/Noticias";
+import { DateTime } from 'luxon';
+import Spinner from 'react-bootstrap/Spinner';
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
@@ -16,6 +19,7 @@ const Search = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setLoading(true);
     // Realizar la búsqueda aquí utilizando la API de NewsAPI
   }
 
@@ -25,8 +29,11 @@ const Search = () => {
         `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=93ec34e03d6f4d769b150cc027fc47ee&page=1&language=es&pageSize=10&language=es`
       );
       setSearchResults(result.data.articles);
+      setLoading(false);
     };
-    fetchData();
+    if (searchTerm) {
+      fetchData();
+    }
   }, [searchTerm]);
 
   return (
@@ -37,19 +44,25 @@ const Search = () => {
           value={searchTerm}
           onChange={handleChange}
         />
-        <button type="submit">Search</button>
+        <button type="submit" disabled={searchTerm.length <=3}>Buscar</button>
       </form>
-      {searchResults.map((result) => (
-        <div key={result.title}>
-          <h3>{result.title}</h3>
-          <p>{result.description}</p>
-          <a href={result.url}>Read more</a>
-        </div>
-      ))}
+      {loading ? (
+        <Spinner animation="border" />
+      ) : (
+        searchResults.map((result) => (
+          <div key={result.title}>
+            <h3>{result.title}</h3>
+            <p>{result.description}</p>
+            <p>
+              Fecha de publicación: {DateTime.fromISO(result.publishedAt).toLocaleString(DateTime.DATE_FULL)}
+            </p>
+            <a href={result.url} target="_blank" rel="noopener noreferrer">Leer más</a>
+          </div>
+        ))
+      )}
     </div>
   );
 };
 
 export default Search;
-
 
