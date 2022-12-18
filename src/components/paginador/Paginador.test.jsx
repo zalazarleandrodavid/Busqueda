ea16e83b4d256d8347ef2ea43a1b1bde08312285
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2022-12-17 22:09:36 -0300

    test 2

--- src/components/paginador/Paginador.test.jsx
+++ src/components/paginador/Paginador.test.jsx
@@ -12,33 +12,4 @@ describe('Paginador', () => {
     const { getByText } = render(<Paginador currentPage={2} />);
     expect(getByText('Anterior')).toBeInTheDocument();
   });
-
-  it('no debería mostrar el botón "Anterior" si la página actual es 1', () => {
-    const { queryByText } = render(<Paginador currentPage={1} />);
-    expect(queryByText('Anterior')).not.toBeInTheDocument();
-  });
-
-  it('debería mostrar el botón "Siguiente" si la página actual es menor que el total de páginas', () => {
-    const { getByText } = render(<Paginador currentPage={1} totalPages={2} />);
-    expect(getByText('Siguiente')).toBeInTheDocument();
-  });
-
-  it('no debería mostrar el botón "Siguiente" si la página actual es igual al total de páginas', () => {
-    const { queryByText } = render(<Paginador currentPage={2} totalPages={2} />);
-    expect(queryByText('Siguiente')).not.toBeInTheDocument();
-  });
-
-  it('debería llamar a la función "handlePageChange" al hacer clic en el botón "Anterior"', () => {
-    const handlePageChange = jest.fn();
-    const { getByText } = render(<Paginador currentPage={2} handlePageChange={handlePageChange} />);
-    fireEvent.click(getByText('Anterior'));
-    expect(handlePageChange).toHaveBeenCalledWith(1);
-  });
-
-  it('debería llamar a la función "handlePageChange" al hacer clic en el botón "Siguiente"', () => {
-    const handlePageChange = jest.fn();
-    const { getByText } = render(<Paginador currentPage={1} totalPages={2} handlePageChange={handlePageChange} />);
-    fireEvent.click(getByText('Siguiente'));
-    expect(handlePageChange).toHaveBeenCalledWith(2);
-  });
 });
